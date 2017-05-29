c7355853fdaa9364c444f3a32f7dd03284c704fe
Jose Alves <a.sousajose@gmail.com>
2017-05-29 10:35:25 -0400

    Aula 6

---
+++ Codigos/aula6/estilos.php
@@ -0,0 +1,62 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Aula 6</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+	<div id="header" style="text-align:center;padding:5px;heigth:300px">
+		<h1 style="color:#8B4513">blue</h1>
+	</div>
+	<div style="background-color:#eee;line-height:30px;width:100px;weigth:300px;float:left;padding:5px">
+	<ul>
+		<li>1</li>
+		<li>3</li>
+		<li>5</li>
+		<li>2</li>
+	</ul>
+	</div>
+	<div id="left" style="width:350px;float:left;padding:10px">
+	<h1 style="color:#8B4513">blue</h1>
+	
+	<div id="rigth" style="text-align:right;width:100px">
+	<h1 style="color:#8B4513">blue</h1>
+	</div>
+	<h6 style="color:#8B4513;font-size:150%">H6</h6>
+	<p style="font-family:verdana">Verdana</p>
+	<p>
+	Mussum Ipsum, cacilds vidis litro abertis.	Praesent malesuada urna nisi, quis 
+	volutpat erat hendrerit non.  	Nam vulputate dapibus. Quem num gosta di mé, 	boa gentis num é. <span style="color:#f0f;font-size:150%;font-style:oblique">Per aumento de cachacis</span>, 
+	eu reclamis. Todo mundo vê os porris que 
+	eu tomo, mas ninguém vê os tombis que eu levo!
+	</p>
+	
+
+	<ol type="I" >
+		<li>1</li>
+		<li>3</li>
+		<li>5</li>
+		<li>2</li>
+	</ol>
+	
+	<dl>
+		<dt>1dsa</dt>
+		<dd>dasdlsja lasdkljasld</dd>
+		<dd>dasdlsja lasdkljasld</dd>
+	</dl>
+	
+</div>
+<div id="footer" style="background-color:black;color:white;clear:both;text-align:center;padding:5px">
+pagina
+</div>
+
+<article>
+csfdsf 
+asdas
+</article>
+<aside>
+sdadsadasdas
+</aside>
+
+	</body>
+</html>
