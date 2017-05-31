941dee1b24f4c272eda135eda0ceeecdb25d132f
Jose Alves <a.sousajose@gmail.com>
2017-05-31 10:29:57 -0400

    template bootstrap

---
+++ template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title> </title>
+		<meta  charset="utf-8" name="viewport" content="width=device-width,initial-scale=1">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+	</head>
+	<body>
+
+	</body>
+</html>
